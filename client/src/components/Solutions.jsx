49c8e0e43e7149082fed26bdcbc3a9358c7a72c2
Iman Bashir <imanbashir@gmail.com>
2020-02-19 16:31:44 -0500

    Started Solutions page body

--- client/src/components/Solutions.jsx
+++ client/src/components/Solutions.jsx
@@ -8,11 +8,35 @@ const Solutions = () => {
         <div id="title-inner-pages">Air Quality Solutions</div>
         </div>
         <div className="solutions-body">
-          Good Moderate Hazardous Toxic
+          <h10>
+            1'st Step is Knowing... AQI What?
+            </h10>
           <p>
-            Here we can put Solutions for Moderate, Hazardous, Toxic pollution
-            from website.
-          </p>
+            AQI, also known as Air Quality Index, is an index for reporting daily air quality. It tells you how clean or polluted your air is, and what associated health effects might be.
+            <br></br><br></br><b>Good</b> - 0 to 50	
+            <br></br><b>Moderate</b> - 51 to 100 
+            <br></br><b>Unhealthy for Sensitive Groups</b> - 101 to 150	
+            <br></br><b>Unhealthy</b> - 151 to 200
+            <br></br><b>Very Unhealthy</b> - 201 to 300
+            <br></br><b>Hazardous</b> - 301-500
+            <br></br><br></br>
+            Read more on what each means here: <a href="https://airnow.gov/index.cfm?action=aqibasics.aqi">Air Quality Index</a>
+            </p>
+            <br></br><br></br>
+            <h10>Some Every Day Solutions</h10>
+            <br></br>
+            <h9>Plants</h9>
+            <p>
+            insert info about plants here
+              </p>
+            <h9>Exercise</h9>
+            <p>
+            insert info about exercise here
+              </p>
+            <h9>Better Ventilation</h9>
+            <p>
+              insert info about better Ventilation here
+              </p>
         </div>
       <div className="bottom-image">
         <div className="bottom-hero-text">
